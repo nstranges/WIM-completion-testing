104c15a87e4975b9670a161e8692245ef415554d
nstranges <nicholastranges4@hotmail.com>
2025-06-16 22:19:16 -0400

    Adding all of the created files

---
+++ testing-completions.py
@@ -0,0 +1,60 @@
+import re
+from trl import HfPairwiseJudge, OpenAIPairwiseJudge
+from datasets import load_dataset
+import json
+import os
+import time
+
+# The model testing the win rate
+judge_model = 'gpt-4o-mini'
+
+# Load the answers
+with open("wim1.0-completions.txt", "r", encoding="utf-8") as f:
+    raw = f.read()
+model_completions = re.findall(r"\{\n(.*?)\n\}", raw, flags=re.DOTALL)
+print('Loaded the completions')
+
+# Load the dataset
+num_examples = 1000
+dataset = load_dataset("trl-lib/tldr", split="validation")
+if num_examples is not None:
+    dataset = dataset.select(range(num_examples))
+
+# Reference completions
+prompts = dataset["prompt"]
+reference_completions = dataset["completion"]
+print('Loaded the datasets')
+
+# Load the openai API token
+with open('openai_token.json', 'r') as config_file:
+    config = json.load(config_file)
+
+os.environ["OPENAI_API_KEY"] = config['token']
+
+# Judge the outputs
+if "gpt" in judge_model:
+    judge = OpenAIPairwiseJudge(judge_model)
+else:
+    judge = HfPairwiseJudge(judge_model)
+
+# Get all of the completions
+batch_size = 5
+delayed_results = []
+total_done = 0
+completions = [[c0, c1] for c0, c1 in zip(reference_completions, model_completions)]
+
+# Loop through these in batches
+for i in range(0, len(prompts), batch_size):
+    prompt_batch = prompts[i:i + batch_size]
+    completion_batch = completions[i:i + batch_size]
+    
+    result = judge.judge(prompt_batch, completion_batch)
+    delayed_results.extend(result)
+
+    total_done += batch_size
+    print(f'Done {total_done} / {num_examples}')
+    time.sleep(2)
+
+best_idxs = delayed_results
+model_win_rate = best_idxs.count(1) / len(best_idxs)
+print(f"Model win rate: {model_win_rate * 100:.2f}%")
